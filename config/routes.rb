84fcca04707d8dce49fc001278c67844f844d418
Marcos <marcos@asseinfo.com.br>
2015-07-01 12:43:20 -0300

    Configure Product at routes' file.

--- config/routes.rb
+++ config/routes.rb
@@ -53,4 +53,11 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
+
+  Rails.application.routes.draw do
+    namespace :api do
+      resources :products
+    end
+  end
+
 end
