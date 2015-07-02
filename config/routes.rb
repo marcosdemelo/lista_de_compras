f5a64be9fc8b5759a51035a9ac3f63ceccd890d6
Marcos <marcos@asseinfo.com.br>
2015-07-01 22:02:04 -0300

    Create List's model.

--- config/routes.rb
+++ config/routes.rb
@@ -60,4 +60,5 @@ Rails.application.routes.draw do
     end
   end
 
+  resources :lists
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150701153937) do
+ActiveRecord::Schema.define(version: 20150702010059) do
+
+  create_table "lists", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "products", force: :cascade do |t|
     t.string   "name"
